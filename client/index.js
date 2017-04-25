ce4643f0af4f732a815905089d42541a6d252eb1
neurotoxinvx <neurotoxin@vip.qq.com>
2017-04-26 01:44:05 +0800

    change the rule of load resource

--- client/index.js
+++ client/index.js
@@ -2,18 +2,16 @@ function _client_(options) {
   this.options = options;
   this.entry = window._LSOffline_.entry;
 
-  this.lib = {
-    length: 0
-  };
+  this.queue = []
 
   this.runLoader()
 }
 
 _client_.prototype = {
   storage: {
-    test: function () {
+    canIUse: function () {
       try {
-        var testKey = '_storeJS_'
+        var testKey = '_storageTest_'
         this.set(testKey, testKey)
 
         if (this.get(testKey) !== testKey) {
@@ -60,30 +58,32 @@ _client_.prototype = {
   requestJS: function(params, callback, fallback){
     try{
       var xhr = new XMLHttpRequest();
-      xhr.open("get", params.path, true);
+
+      xhr.open("GET", params.path, true);
       xhr.onreadystatechange = function(){
         if (xhr.readyState == 4){
           if((xhr.status >= 200 && xhr.status < 300 ) || xhr.status === 304){
-            if(xhr.response != ''){
+            if(!!xhr.response){
               callback(params, xhr.response);
             }
           }
         }
       };
+
       xhr.send(null);
     } catch (error){
       fallback(params, error);
-      throw new Error(error);
     }
   },
   createLoad: function(key) {
     var self = this;
 
+    self.queue.push({key: key, code: '', status: 'loading'});
+
     self.getResource(key, function(code) {
-      self.runTogether(key, code);
+      self.runQueue(key, code);
     }, function(params, error) {
-      self.appendScriptTag('', params.path);
-      throw new Error(error);
+      console.log(params.path + ' can not load, because: ' + JSON.stringify(error));
     })
   },
   getResource: function(key, callback, fallback) {
@@ -94,9 +94,9 @@ _client_.prototype = {
         key: key,
         path: key
       }, function(params, response) {
-        self.storage.set(params.key, response);
-
         callback(response);
+
+        self.storage.set(params.key, response);
       }, function(params, error) {
         fallback(params, error);
       })
@@ -104,24 +104,37 @@ _client_.prototype = {
       callback(self.storage.get(key));
     }
   },
-  runTogether: function(key, code) {
+  runQueue: function(key, code) {
     var self = this;
 
-    self.lib[key] = code;
-    self.lib.length += 1;
-
-    if (self.lib.length === self.entry.length) {
-      for (var i = 0; i < self.entry.length; i++) {
-        var item = self.entry[i];
-        self.appendScriptTag(self.lib[item]);
+    if (!!key && !!code) {
+      for (var k in self.queue) {
+        if (self.queue[k].key === key) {
+          self.queue[k].code = code;
+          self.queue[k].status = 'loaded';
+        }
       }
     }
+
+    if (!!self.queue[0] && !!self.queue[0].code && self.queue[0].status === 'loaded') {
+      self.queue[0].status = 'appending';
+
+      self.appendScriptTag(self.queue[0].code, null, function () {
+        self.queue.status = 'appended';
+        self.queue.shift();
+
+        if (self.queue.length > 0) {
+          self.runQueue();
+        }
+      });
+    }
   },
-  appendScriptTag: function(code, src) {
+  appendScriptTag: function(code, src, loadedCallBack) {
     var script = document.createElement('script');
 
     if (src) {
       script.src = src;
+      script.onload = loadedCallBack;
     }
 
     if (code) {
@@ -133,7 +146,7 @@ _client_.prototype = {
   runLoader: function() {
     var self = this;
 
-    this.storage.test();
+    this.storage.canIUse();
 
     for (var i = 0; i < self.entry.length; i++) {
       self.createLoad(self.entry[i]);
